08b5db45a72cf5a89b28ead62a802795c867391a
huuuynh <huynhanhtang94400@gmail.com>
2024-11-19 12:21:05 +0100

    Update 2048.cpp
    Changement fonction Plateau Vide et ajout plateau initial + affichage

--- 2048.cpp
+++ 2048.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <random>
 using namespace std;
 
 using Plateau = vector<vector<int>>;
@@ -18,11 +19,11 @@ int tireDeuxOuQuatre(){
 Plateau plateauVide(){
     Plateau t;
     t = Plateau(4);
-    for (int i = 0; t.size(); i++){
+    for (int i = 0; i < 4; i++){
         t[i] = vector<int>(4);
     }
     for (int i = 0; i < 4; i++){
-        for (int k = 0; k < 4; k++){
+        for (int k = 0; t[i].size() < 4; k++){
             t[i][k] = 0;
         }
     }
@@ -33,8 +34,23 @@ Plateau plateauInitial(){
     Plateau t;
     t = plateauVide();
     int place = 0;
-
+    while(place != 2){
+        int ligne = 0 + rand() % 4;
+        int colonne = 0 + rand() % 4;
+        t[ligne][colonne] = tireDeuxOuQuatre();
+        place++;
+    }
+    return t;
+}
+void affichePlateauTab(Plateau t){
+    for (int i = 0; i < t.size();i++){
+        for (int k = 0; k < t[i].size(); k++){
+            cout << t[i][k] << " ";
+        }
+        cout << endl;
+    }
 }
 int main(){
-
+    affichePlateauTab(plateauVide());
+    affichePlateauTab(plateauInitial());
 }
