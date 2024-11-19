5cd01dc0a8af859585e91059a265c5bce56cba9d
absrme <4gsssst3@gmail.com>
2024-11-19 12:24:10 +0100

    TEST.cpp

--- TEST.cpp
+++ TEST.cpp
@@ -4,4 +4,13 @@
 #include <random>
 using namespace std;
 using Plateau = vector<vector<int>>;
-
+Plateau déplacementDroite(Plateau plateau){
+for (int i = 0; i<plateau.size(); i++){
+        for (int j = 0; j<plateau[i].size(); j++){
+            if (plateau[i+1][j+1] == 0 or plateau[i][j]==plateau[i+1][j+1]){
+                plateau[i+1][j+1] = plateau[i][j];
+                plateau[i][j]==0;
+            }
+        }
+    }
+}
