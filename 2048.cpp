99c4e460eac7cf7f0fdd1ebf7ec58f4561f5a123
absrme <4gsssst3@gmail.com>
2024-11-19 12:26:59 +0100

    2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -50,6 +50,27 @@ void affichePlateauTab(Plateau t){
         cout << endl;
     }
 }
+
+Plateau déplacementDroite(Plateau plateau){
+     for (int i = 0; i<plateau.size(); i++){
+        for (int j = 0; j<plateau[i].size(); j++){
+            if (plateau[i+1][j+1] == 0 or plateau[i][j]==plateau[i+1][j+1]){
+                plateau[i+1][j+1] = plateau[i][j];
+                plateau[i][j]==0;
+            }
+        }
+    }
+}
+Plateau déplacementHaut(Plateau plateau){
+    for (int i = 0; i<plateau.size(); i++){
+        for (int j = 0; j<plateau[i].size(); j++){
+            if (plateau[i+1][j] == 0 or plateau[i][j]==plateau[i+1][j]){
+                plateau[i+1][j] = plateau[i][j];
+                plateau[i][j]==0;
+            }
+        }
+    }
+}
 int main(){
     affichePlateauTab(plateauVide());
     affichePlateauTab(plateauInitial());
