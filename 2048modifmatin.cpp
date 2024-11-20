292bf0354601f5b06797cf85b60358469fdb0860
huuuynh <huynhanhtang94400@gmail.com>
2024-11-20 08:07:09 +0100

    Update and rename 2048(modifdusoir).cpp to 2048modifmatin.cpp
    //////////////////////////////////////////////////
    // MODIF DE LA FONCTION DEPLACEMENT DROITE (pour que les cases identiques s'assemblent) 
    // CELLE DE GAUCHE (même chose + balayement différent des indices et fonctionne au top)
    // ajout fonction dessinebis je retente dessine + tard
    // ajout fonction estgagnant pas testé
    //////////////////////////////////////////////////

--- 2048modifmatin.cpp
+++ 2048modifmatin.cpp
@@ -3,16 +3,16 @@
 #include <string>
 #include <iostream>
 #include <random>
+#include <iomanip>
 using namespace std;
 using Plateau = vector<vector<int>>;
 
 //////////////////////////////////////////////////
-// LE PROGRAMME FONCTIONNE MAIS IL EST PAS TOP SUR LE RANDOM ET LE DEPLACEMENT
-// L'ESSENTIEL Y EST MAIS JE SUIS TROP FATIGUé POUR CONTINUER
-// COMPILE LE POUR VOIR LE PROBLEME DE RAND()
-// go fuck yourselv
+// MODIF DE LA FONCTION DEPLACEMENT DROITE (pour que les cases identiques s'assemblent) 
+// CELLE DE GAUCHE (même chose + balayement différent des indices et fonctionne au top)
+// ajout fonction dessinebis je retente dessine + tard
+// ajout fonction estgagnant pas testé
 //////////////////////////////////////////////////
-
 int tireDeuxOuQuatre(){
     int proba = 1 + rand() % 11;
     if (proba <= 9){
@@ -30,7 +30,7 @@ Plateau plateauVide(){
         t[i] = vector<int>(4);
     }
     for (int i = 0; i < 4; i++){
-        for (int k = 0; t[i].size() < 4; k++){
+        for (int k = 0; k < 4; k++){
             t[i][k] = 0;
         }
     }
@@ -42,7 +42,7 @@ Plateau plateauInitial(){
     int ligne;
     int colonne;
     t = plateauVide();
-    for(int i = 0; i<2; i++){ //2 tours de boucle car 2 cases aléatoires
+    for(int i = 0; i < 2; i++){ //2 tours de boucle car 2 cases aléatoires
         ligne = rand() % 3; //indice de ligne aléatoire entre 0 et 3
         colonne = rand() % 3; //indice de colonne aléatoire entre 0 et 3
         t[ligne][colonne] = tireDeuxOuQuatre(); //la case du tableau aléatoire
@@ -60,10 +60,14 @@ void affichePlateau(Plateau t){
 
 Plateau déplacementDroite(Plateau plateau){
      for (int i = 0; i<plateau.size(); i++){
-        for (int j = 0; j<plateau[i].size()-1; j++){
-            if (plateau[i][j+1] == 0 or plateau[i][j]==plateau[i][j+1]){
+        for (int j = 0; j < plateau[i].size()-1; j++){
+            if (plateau[i][j] == plateau[i][j+1]){
+                plateau[i][j+1] = plateau[i][j+1] * 2;
+                plateau[i][j] = 0;
+            }
+            if (plateau[i][j+1] == 0 and plateau[i][j] != 0){
                 plateau[i][j+1] = plateau[i][j];
-                plateau[i][j]=0;
+                plateau[i][j] = 0;
             }
         }
     }
@@ -71,8 +75,12 @@ Plateau déplacementDroite(Plateau plateau){
 }
 
 Plateau déplacementGauche(Plateau plateau){
-     for (int i = 0; i<plateau.size(); i++){
-        for (int j = 1; j<plateau[i].size(); j++){
+     for (int i =plateau.size() - 1; i >= 0; i--){
+        for (int j = plateau[i].size() - 1; j >= 1; j--){
+            if (plateau[i][j-1] == plateau[i][j]){
+                plateau[i][j-1] = plateau[i][j-1] * 2;
+                plateau[i][j] = 0;
+            }
             if (plateau[i][j-1] == 0 or plateau[i][j-1]==plateau[i][j]){
                 plateau[i][j-1] = plateau[i][j];
                 plateau[i][j]=0;
@@ -131,17 +139,54 @@ Plateau déplacement(Plateau plateau, char Touche){
     cout << "La touche n'est pas reconnue..." << endl;
     return plateau;
 }
+void dessinebis(Plateau p){
+    int espacement = 5;
+    for (int ligne = 0; ligne < p.size(); ligne++){
+        for (int i = 0; i < espacement*4 + espacement; i++){
+            cout << "*";
+        }
+        cout << endl;
+        for (int colonne = 0; colonne < p[0].size(); colonne++){
+            if (p[ligne][colonne] == 0){
+                cout << "*" << setw(espacement) << " ";
+            }
+            else{
+                cout << "*" << setw(espacement) << p[ligne][colonne];
+            }
+        }
+        cout << "*" << endl;
+    }
+    for (int i = 0; i < espacement*4 + espacement; i++){
+            cout << "*";
+        }
+    cout << endl;
+}
 
-
+bool estGagnant(Plateau plateau){
+    for(int i = 0; i < plateau.size(); i++){
+        for (int k = 0; k < plateau[0].size(); k++){
+            if (plateau[i][k] == 2048){
+                return true;
+            }
+        }
+    }
+    return false;
+}
 int main(){
     char Touche;
     cout << "Test rapide que les fonctions marchent..." << endl;
     cout << "Affichage du plateau vide..." << endl;
-    affichePlateau(plateauVide());
+    dessinebis(plateauVide());
     cout << "Affichage du plateau initial..." << endl;
-    affichePlateau(plateauInitial());
+    Plateau t2;
+    Plateau t4;
+    t2 = plateauInitial();
+    t4 = plateauInitial();
+    dessinebis(t2);
+    dessinebis(t4);
     cout << "Ok, maintenant on teste de déplacer le tableau..." << endl;
     cout << "Entre Z,Q,S ou D pour déplacer le tableau..." << endl;
     cin >> Touche;
-    affichePlateau(déplacement(plateauInitial(),Touche));
+    dessinebis(déplacement(t4,Touche));
+    dessinebis(déplacement(t2,Touche));
 }
