8334f74c7c62e1e6ed8a70eced26e9f904d48711
huuuynh <huynhanhtang94400@gmail.com>
2024-12-03 21:24:48 +0100

    Ajouts dans fonctions déplacement pour éviter des coups en trop

--- main.cpp
+++ main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <stdlib.h>
 #include "2048.hpp"
 using namespace std;
 
 
 int main(){
-    ASCII2048();
-    this_thread::sleep_for(chrono::milliseconds(800));
-    Tutoriel();
-    // Déroulement d'une partie 
-    Plateau t;
-    char Touche; 
-    t = plateauInitial();
-    dessinebis(t);
-    while(ConditionFinDeJeu(t)){
-        cout << "Choisi une touche entre Z,Q,S,D !" << endl;
-        cin >> Touche;
-        cout << "Voici le plateau après ton déplacement !" << endl;
-        t = déplacement(t,Touche);
-        dessinebis(t);
-        cout << "Voici le tableau après un placement aléatoire !" << endl;
-        t = plateauPlacementAléatoire(t);
-        dessinebis(t);
-    }
-    // testVilain();
+    // ASCII2048();
+    // this_thread::sleep_for(chrono::milliseconds(800));
+    // Tutoriel();
+    // // Déroulement d'une partie 
+    // Plateau t;
+    // char Touche; 
+    // t = plateauInitial();
+    // dessinebis(t);
+    // while(ConditionFinDeJeu(t)){
+    //     cout << "Choisi une touche entre Z,Q,S,D !" << endl;
+    //     cin >> Touche;
+    //     cout << "Voici le plateau après ton déplacement !" << endl;
+    //     t = déplacement(t,Touche);
+    //     dessinebis(t);
+    //     cout << "Voici le tableau après un placement aléatoire !" << endl;
+    //     t = plateauPlacementAléatoire(t);
+    //     dessinebis(t);
+    // }
+    testVilain();
+    // HighScoreTXT();
 }
---
+++ ncurses.cpp
@@ -0,0 +1,15 @@
+#include <curses.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int c;
+    keypad(stdscr, true);
+    printw("Bonjour!\nIl est temps de presser quelques touches au clavier...\n");
+    do {
+        c = getch();
+        addch(c);
+    } while (c != KEY_UP);
+    return 0;
+}
+
